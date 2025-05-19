724ac3ae6a8dd91df55f1bf2e5bcf8032d5af334
Shinta Nursobah Chairani <shinta.nur505@gmail.com>
2025-05-19 19:14:11 +0700

    Update NoteRoute.js

--- backend/routes/NoteRoute.js
+++ backend/routes/NoteRoute.js
@@ -4,17 +4,8 @@ import {
     getNoteById,
     createNotes,
     updateNotes,
-    deleteNotes,
-    getUsers,
-    createUser,
-    updateUser,
-    deleteUser,
-    getUserById,
-    login,
-    logout,
+    deleteNotes
 } from "../controllers/NoteController.js";
-import { verifyToken } from "../middleware/VerifyToken.js";
-import { getAccessToken } from "../controllers/TokenController.js";
 
 const router = express.Router();
 
@@ -23,15 +14,5 @@ router.get("/note/:id", getNoteById);
 router.post("/add-notes", createNotes);
 router.put("/note/:id", updateNotes);
 router.delete("/note/:id", deleteNotes);
-
-router.get("/token", getAccessToken);
-router.post("/login", login);
-router.delete("/logout", logout);
-
-router.get("/users", verifyToken, getUsers);
-router.get("/users/:id", verifyToken, getUserById);
-router.post("/users", createUser);
-router.put("/users/:id", verifyToken, updateUser);
-router.delete("/users/:id", verifyToken, deleteUser);
 
 export default router;
